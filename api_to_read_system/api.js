29d88553ff54efaa0373e41d0deb7e1085559412
AJ Zabel <andrew.zabel@target.com>
2018-01-23 09:13:21 -0600

    updates

--- api_to_read_system/api.js
+++ api_to_read_system/api.js
@@ -63,10 +63,11 @@ function getEnv() {
       if(data[i].pid != NaN) {
         let env = hello.execSync('cat /proc/' + data[i].pid + '/environ').toString('utf8');
         let varName = env.match(/[A-Z*_*]+\=+/g);
-        if(varName != null) {
-          varValue = varName.map(x => varName.match(x))
-          console.log(varValue.substr(varValue[i].index, varValue[i+1].index - varValue[i].index));
-        }
+        console.log(varName);
+        // if(varName != null) {
+        //   varValue = varName.map(x => varName.match(x))
+        //   console.log(varValue.substr(varValue[i].index, varValue[i+1].index - varValue[i].index));
+        // }
       }
     }
   })
